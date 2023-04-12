5f270a3ea0a04354f9fff6a657c9aeb2bfce69e3
Kaeden Le <kaedenle@gmail.com>
2023-04-12 03:22:05 -0400

    changes to rover to include top and bottom 5 as well as a linked list

--- Rover.java
+++ Rover.java
@@ -5,6 +5,138 @@ import java.nio.file.Paths;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+class Data{
+	public int minute;
+	public int data;
+	public Data(int minute, int data) {
+		this.minute = minute;
+		this.data = data;
+	}
+}
+class Node {
+	Data item;
+	int key;
+	Node next;
+	Node prev;
+	private Lock locks = new ReentrantLock();
+	public Node(int key) {
+		item = new Data(0, key);
+		this.key = key;
+		next = null;
+		prev = null;
+	}
+	public Node(int key, int time) {
+		item = new Data(time, key);
+		this.key = key;
+		next = null;
+		prev = null;
+	}
+	public void lock() {
+		locks.lock();
+	}
+	public void unlock() {
+		locks.unlock();
+	}
+}
+class ConLinkedList{
+	private Node head;
+	private Node tail;
+	boolean uniqueValues = true;
+	public ConLinkedList(){
+		head = new Node(Integer.MIN_VALUE);
+		head.next = new Node(Integer.MAX_VALUE);
+		head.prev = null;
+		
+		tail = head.next;
+		tail.prev = head;
+	}
+	public String getUpToIndexHead(int index) {
+		String ret = "\n";
+		Node curr = head;
+		int counter = 0;
+		while(curr.next != null && counter < index) {
+			curr = curr.next;
+			ret += curr.key + "F (Minute "+curr.item.minute+")\n";
+			counter++;
+		}
+		return ret;
+	}
+	public String getUpToIndexTail(int index) {
+		String ret = "\n";
+		Node curr = tail;
+		int counter = 0;
+		while(curr.prev != null && counter < index) {
+			curr = curr.prev;
+			ret += curr.key + "F (Minute "+curr.item.minute+")\n";
+			counter++;
+		}
+		return ret;
+	}
+	public boolean add(int item, int minute) {
+		int key = item;
+		head.lock();
+		Node pred = head;
+		try {
+			//inital lock
+			Node curr = pred.next;
+			curr.lock();
+			try {
+				//seeking phase
+				while(curr.key < key) {
+					pred.unlock();
+					pred = curr;
+					curr = curr.next;
+					curr.lock();
+				}
+				//if already in list (uncomment to get top 5 unique)
+				if(curr.key == key && uniqueValues) {
+					return false;
+				}
+				Node newNode = new Node(item, minute);
+				newNode.next = curr;
+				curr.prev = newNode;
+				newNode.prev = pred;
+				pred.next = newNode;
+				return true;
+			}finally {
+				curr.unlock();
+			}
+		}
+		finally {
+			pred.unlock();
+		}
+	}
+	public boolean remove(int item) {
+		Node pred = null, curr = null;
+		head.lock();
+		try {
+			pred = head;
+			curr = pred.next;
+			curr.lock();
+			try {
+				while(curr.key < item) {
+					pred.unlock();
+					pred = curr;
+					curr = curr.next;
+					curr.lock();
+				}
+				//the magic
+				if(curr.key == item) {
+					pred.next = curr.next;
+					curr.prev = pred;
+					return true;
+				}
+				return false;
+			} finally {
+				curr.unlock();
+			}
+		}finally {
+			pred.unlock();
+		}
+	}
+}
 
 class TempMod implements Runnable{
 	public int minute;
@@ -13,9 +145,6 @@ class TempMod implements Runnable{
 	public static int[] Memory;
 	public static int N;
 	private static int hours;
-	//set numbers out of range
-	public static AtomicInteger lowest = new AtomicInteger(200);
-	public static AtomicInteger highest = new AtomicInteger(-200);
 	public static AtomicInteger diff = new AtomicInteger(0);
 	public static AtomicInteger timeDiff = new AtomicInteger(0);
 	public static int IDContr = 0;
@@ -25,6 +154,7 @@ class TempMod implements Runnable{
 	private int startingIndex;
 	private int endingIndex;
 	public static String output;
+	private static ConLinkedList ll;
 	
 	public TempMod(int ID) {
 		minute = 0;
@@ -37,6 +167,7 @@ class TempMod implements Runnable{
 		Memory = new int[N * 60];
 		hours = hour;
 		output = "";
+		ll = new ConLinkedList();
 	}
 	private void GetOperableRange() {
 		startingIndex = ID * 60;
@@ -46,15 +177,10 @@ class TempMod implements Runnable{
 	private boolean greaterThanCompareAndSet(int newValue, AtomicInteger oldval) {
 		return oldval.updateAndGet(x -> x < newValue ? newValue : x) == newValue;
 	}
-	private boolean lessThanCompareAndSet(int newValue, AtomicInteger oldval) {
-		return oldval.updateAndGet(x -> x > newValue ? newValue : x) == newValue;
-	}
 	private void Calculate() {
 		for(int i  = startingIndex; i <= endingIndex; i++)
 		{
-			greaterThanCompareAndSet(Memory[i], highest);
-			lessThanCompareAndSet(Memory[i], lowest);
-			//test the 10 minute intervals (if you have one)
+			ll.add(Memory[i], (i % 60) + 1);
 			if(i + 10 < N*60) {
 				synchronized(this) {
 					if(greaterThanCompareAndSet(Math.abs(Memory[i] - Memory[i + 10]), diff))
@@ -62,13 +188,15 @@ class TempMod implements Runnable{
 						timeDiff.set((i % 60) + 1);
 						IDContr = ID;
 					}
-						
 				}
 			}		
 		}
 	}
 	public static void OutputReport(int count) {
-		String out = "-----------------------HOUR " + count + " METRICS-----------------------\n" + "LOWEST: " + lowest + "\nHIGHEST: " + highest + "\nDIFFERENCE: " + diff + " (between " + timeDiff + " and " + (timeDiff.get() + 10) + "ms ID "+ IDContr +")\n";
+		String bottom5 = ll.getUpToIndexHead(5);
+		String top5 = ll.getUpToIndexTail(5);
+		String out = "-----------------------HOUR " + count + " METRICS-----------------------\n" + "LOWEST: " + bottom5 + 
+				"\nHIGHEST: " + top5 + "\nDIFFERENCE: " + diff + " (between " + timeDiff + " and " + (timeDiff.get() + 10) + " ID "+ IDContr +")\n";
 		System.out.print(out);
 		output += out;
 	}
@@ -97,14 +225,22 @@ class TempMod implements Runnable{
 			output += out;
 			//spin until all threads have caught up
 			if(counter.incrementAndGet() < N) {
-				while(counter.get() != 0) {}
+				//insert short delay to not crash computer
+				while(counter.get() != 0) {
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
 			}
 			else
 			{
 				OutputReport(hourCount + 1);
-				lowest.set(200);
-				highest.set(-200);
+				//reset values
 				diff.set(0);
+				ll = new ConLinkedList();
 				counter.set(0);	
 			}
 			minute = 0;
@@ -114,7 +250,6 @@ class TempMod implements Runnable{
 	}
 }
 public class Rover {
-
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		System.out.println("How many hours to simulate?");
@@ -123,7 +258,7 @@ public class Rover {
 		int time = s.nextInt();
 		long startTime = System.nanoTime();
 		//System.out.println("The Starting Value is: " + startingValue);
-		int N = 8;
+		int N = 1;
 		Thread[] tList = new Thread[N];
 		
 		//setup all the threads
@@ -146,7 +281,9 @@ public class Rover {
 			System.out.println(e);
 		}
 		long endTime = System.nanoTime();
-		TempMod.output += "Full program took " + ((endTime - startTime)/1000000f) + "ms to execute";
+		String out = "Full program took " + ((endTime - startTime)/1000000f) + "ms to execute";
+		System.out.println(out);
+		TempMod.output += out;
 		Path fileName = Path.of(Paths.get("").toAbsolutePath().toString() + "_output.txt");
 		try {
 			Files.writeString(fileName, (TempMod.output));
@@ -154,7 +291,6 @@ public class Rover {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        s.close();
 		
 	}
 
